d4ba67c92d842dda95d1e57f70b4fb292342a5f4
merry-trip <contact@merry-trip.com>
2025-04-02 15:01:19 +0900

    Fix keyFilePath resolution

--- app/api/sheets.js
+++ app/api/sheets.js
@@ -1,16 +1,22 @@
 // api/sheets.js
 const { google } = require('googleapis');
 const path = require('path');
+const fs = require('fs');
 require('dotenv').config();
 
 // 環境（dev or prod）を読み取る
 const appEnv = process.env.APP_ENV || 'dev';
 
-// 認証ファイルのパスを切り替え
-const keyFilePath = appEnv === 'prod'
-  ? process.env.GOOGLE_CREDENTIALS_JSON_PROD
-  : process.env.GOOGLE_CREDENTIALS_PATH_DEV;
+let keyFilePath;
 
+if (appEnv === 'prod') {
+  const jsonContent = process.env.GOOGLE_CREDENTIALS_JSON_PROD;
+  const tempPath = path.resolve(__dirname, '../credentials.prod.json');
+  fs.writeFileSync(tempPath, jsonContent);
+  keyFilePath = tempPath;
+} else {
+  keyFilePath = path.resolve(process.env.GOOGLE_CREDENTIALS_PATH_DEV);
+}
 // スプレッドシートのIDとシート名を切り替え
 const spreadsheetId = appEnv === 'prod'
   ? process.env.SHEET_ID_WEATHER_PROD
@@ -22,7 +28,7 @@ const sheetName = appEnv === 'prod'
 
 // 認証の設定（Google公式）
 const auth = new google.auth.GoogleAuth({
-  keyFile: path.resolve(keyFilePath),
+  keyFile: keyFilePath
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
