5265c9d2ee279362c12858072251c7dac4ee2cd7
merry-trip <contact@merry-trip.com>
2025-04-29 17:05:14 +0900

    🧹 WordPress不要ファイルを削除＆.gitignoreに追加（plugins/languages/uploads）

--- app/public/wp-content/plugins/conoha-wing-plugin/lib/view/assets/js/frame.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-    let _frame = $('#cwp-settings'),
-        _wrap = $('#wpwrap');
-
-    $(window).on('load resize', function () {
-        _frame.css('height', 'auto').css('height', _wrap.innerHeight());
-    });
-})(jQuery);
